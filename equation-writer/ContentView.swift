2965d7425cdf82684aea6cb3be3628a5bad25b64
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-04-29 19:47:06 -0400

    Xcode basic Hello-world SwiftUI iOS app

---
+++ equation-writer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  equation-writer
+//
+//  Created by Jacques Amiot on 2022-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ equation-writer/equation_writerApp.swift
@@ -0,0 +1,17 @@
+//
+//  equation_writerApp.swift
+//  equation-writer
+//
+//  Created by Jacques Amiot on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct equation_writerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
